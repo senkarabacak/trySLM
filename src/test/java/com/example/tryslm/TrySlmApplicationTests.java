3ba43ddc9a910e4854c9fe0b3869e42f4dff197e
dse <dse@ds-s.at>
2022-10-13 19:25:53 +0200

    Initial commit

---
+++ src/test/java/com/example/tryslm/TrySlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tryslm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrySlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
